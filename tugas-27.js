7e4a8e25c47d389ee15949f69015c1bd8b0a2b5c
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-15 15:37:12 +0700

    first commit

---
+++ tugas-27.js
@@ -0,0 +1,12 @@
+const a = 'No';
+const b = 'rumah saya';
+const c = 30;
+const d = 'A';
+
+console.log(`${a} ${b} ${c}${d}`);
+
+const y = 'Jalan pangeran Tirtayasa No';
+const z = 70;
+
+
+console.log(`${y} ${z}`);
